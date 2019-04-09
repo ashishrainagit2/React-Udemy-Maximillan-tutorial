dd1e876b2937374826d74057ba2f5914a6e6052b
Ashish Raina <ashish.raina@bookmyshow.com>
2019-04-09 18:52:45 +0530

    basic props and states

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons :  [
+      { name: 'Max' , age : 28},
+      { name: 'Manu' , age: 29},
+      { name: 'Stephnei', age:  30 }
+    ],
+    otherState : 'some other value'
+  }
+
+  switchNameHandler = () => {
+    //DONT DO THISthis.state.persons[0].name = 'Maximilian';
+      this.setState ({
+        persons :  [
+          { name: 'Maximilian' , age : 28},
+          { name: 'Manu' , age: 29},
+          { name: 'Stephneii', age:  22 }
+        ]
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>  
         <p>This is really working!</p>
-        <Person />
+        <button onClick={this.switchNameHandler}> Switch  Name </button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Racing </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className : 'App'} , React.createElement('h1', null , 'I am react app with difference') )
