378c383189da5e13c44d6d5f275be30c067bef56
Ashish Raina <ashish.raina@bookmyshow.com>
2019-04-10 14:23:15 +0530

    render conditionllly

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'Manu' , age: 29},
       { name: 'Stephnei', age:  30 }
     ],
-    otherState : 'some other value'
+    otherState : 'some other value',
+    showPersons : false
   }
 
   switchNameHandler = (newName) => {
@@ -33,6 +34,12 @@ class App extends Component {
       })
   }
 
+  togglePersonHandler = () => {
+      this.setState ({
+        showPersons : !this.state.showPersons
+      })
+  }
+
   render() {
     const style = {
       backgroundColor : 'white',
@@ -45,18 +52,24 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm React App</h1>  
         <p>This is really working!</p>
-        <button style={style} onClick={() => this.switchNameHandler('Maximilian!!!')}> Switch  Name </button>
-        <Person 
+        <button style={style} onClick={this.togglePersonHandler}> Switch  Name </button>
+        {
+          this.state.showPersons ? 
+          <div>
+            <Person 
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age}/>
-        <Person 
+            <Person 
             name={this.state.persons[1].name} 
             age={this.state.persons[1].age}
             click={this.switchNameHandler.bind(this, 'Max!!!')}
             changed={this.nameChangeHandler}> My Hobbies: Racing </Person>
-        <Person 
+            <Person 
             name={this.state.persons[2].name} 
             age={this.state.persons[2].age}/>
+            </div> : null
+        }
+        
       </div>
     );
     //return React.createElement('div', {className : 'App'} , React.createElement('h1', null , 'I am react app with difference') )
