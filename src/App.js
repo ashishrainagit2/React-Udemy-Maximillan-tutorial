f933a1a2b0efab681073d2730eec4f994e9a15fe
Ashish Raina <ashish.raina@bookmyshow.com>
2019-04-10 14:06:45 +0530

    adding inline css

--- src/App.js
+++ src/App.js
@@ -34,15 +34,21 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor : 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding : '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>  
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximilian!!!')}> Switch  Name </button>
+        <button style={style} onClick={() => this.switchNameHandler('Maximilian!!!')}> Switch  Name </button>
         <Person 
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age}/>
-
         <Person 
             name={this.state.persons[1].name} 
             age={this.state.persons[1].age}
