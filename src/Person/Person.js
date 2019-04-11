b9c8686adf31525fbddc93b9a93aee72d6435713
Ashish Raina <ashish.raina@bookmyshow.com>
2019-04-11 18:02:24 +0530

    useless error boundary

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,6 +2,10 @@ import React from 'react';
 import  styles from './Person.module.css';
 
 const person = (props) => {
+    const rnd = Math.random();
+    if(rnd > 0.1){
+        throw new Error('Something Went Wrong');
+    }
     return (
         <div className={styles.Person}>
             <p onClick={props.click}> I'm a {props.name} and I am {props.age} year old! </p>
