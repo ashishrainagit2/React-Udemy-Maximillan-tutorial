c2527d2b78efc95b2dc95edfd5d5e1f769570409
Ashish Raina <ashish.raina@bookmyshow.com>
2019-04-23 18:49:46 +0530

    added AUX element to wrap multiple JSX elements in return

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,23 @@
 import React , {Component } from 'react';
 import  styles from './Person.module.css';
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
     render(){
         console.log('[Person.js rendering ...]');
-        return (
-            <div className={styles.Person}>
-                <p onClick={this.props.click}> I'm a {this.props.name} and I am {this.props.age} year old! </p>
-                <p> {this.props.children}</p>
-                <input type='text' onChange={this.props.changed} value={this.props.name}/>
-            </div> 
-        )
+        return 
+            <Aux>
+                <div className={styles.Person}>
+                    <p onClick={this.props.click}> I'm a {this.props.name} and I am {this.props.age} year old! </p>
+                    <p> {this.props.children}</p>
+                    <input 
+                        type='text' 
+                        onChange={this.props.changed} 
+                        value={this.props.name}
+                    />
+                </div> 
+            </Aux>
+        ;
     } 
 }
 
