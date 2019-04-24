f1e0fb71417326410d885e24974d5f04a9712d0d
Ashish Raina <ashish.raina@bookmyshow.com>
2019-04-24 12:19:59 +0530

    reference types in classbased and functional components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,21 +1,23 @@
-import React , { useEffect } from 'react';
+import React , { useEffect, useRef } from 'react';
 import styles from './Cockpit.module.css';
 
 const cockpit  = (props) => {
+    const toggleBtnRef = useRef(null);
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         //Http Request ...
-        const timer = setTimeout(() => {
-            alert('saved data to cloud!!');
-        } , 1000);
+        // const timer = setTimeout(() => {
+        //     alert('saved data to cloud!!');
+        // } , 1000);
+        toggleBtnRef.current.click();
         return() => {
-            clearTimeout(timer);
+            //clearTimeout(timer);
             console.log('[Cockpit.js] Cleanup work in useEffect');
         };
     } , []);
 
     useEffect(() => {
-            console.log('[Cockpit.js] 2nd use effect')
+            console.log('[Cockpit.js] 2nd use effect');
             return () => {
                 console.log('[Cockpit.js] Cleanup work in 2nd  useEffect');
             };
@@ -37,6 +39,7 @@ const cockpit  = (props) => {
             <h1>{props.title}</h1>  
             <p className={classes.join(' ')}>This is really working!</p>
             <button 
+            ref={toggleBtnRef}
             className={btnClass}
             onClick={props.clicked}> Toggle Persons 
             </button>
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,14 +5,25 @@ import Aux from '../../../hoc/Aux';
 import PropTypes from 'prop-types';
 
 class Person extends Component {
+    constructor(){
+        super();
+        this.inputElementRef  = React.createRef();
+    }
+    componentDidMount(){
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
     render(){
         console.log('[Person.js rendering ...]');
         return (
             <Aux>
-                    <p onClick={this.props.click}> I'm a {this.props.name} and I am {this.props.age} year old! </p>
-                    <p> {this.props.children}</p>
+                    <p key="i1" onClick={this.props.click}> I'm a {this.props.name} and I am {this.props.age} year old! </p>
+                    <p key="i2"> {this.props.children}</p>
                     <input 
+                        key = "i3"
                         type='text' 
+                        ref={this.inputElementRef}
+                        // ref={(inputEl) => {this.inputElement = inputEl}}
                         onChange={this.props.changed} 
                         value={this.props.name}
                     />
